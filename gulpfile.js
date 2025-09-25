ac0a03a0e91390d3893cc809f2efe65bfcbbe62f
Luis <Luis_Antonio>
2025-09-25 13:08:39 -0600

    empezando el proyecto con Sass

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+import {src, dest, watch}from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+
+    src('src/scss/app.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('dist/css'))
+
+    done()
+}
+
+export function dev(){
+    //watch('src/scss/app.scss',css)  //un archivo en especifico 
+    watch('src/scss/**/*.scss',css) //le decimos para que compile todos los archivos de scss 
+}
