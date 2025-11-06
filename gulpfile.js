bb0c093d41ff0550e50f751beb6af70addbc4925
Luis <Luis_Antonio>
2025-11-06 11:41:16 -0600

    proyecto terminado

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-import {src, dest, watch}from 'gulp'
+import {src, dest, watch, series}from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+export function js(done){
+
+    src('src/js/app.js')
+    .pipe(dest('dist/js'))
+
+    done()
+}
+
 export function css(done){
 
-    src('src/scss/app.scss')
+    src('src/scss/app.scss', {sourcemaps:true})
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('dist/css'))
+    .pipe(dest('dist/css'), {sourcemaps:true})
 
     done()
 }
@@ -16,4 +24,7 @@ export function css(done){
 export function dev(){
     //watch('src/scss/app.scss',css)  //un archivo en especifico 
     watch('src/scss/**/*.scss',css) //le decimos para que compile todos los archivos de scss 
+    watch('src/js/**/*.js',js) //le decimos para que compile todos los archivos de scss 
 }
+
+export default series(js,css,dev)
